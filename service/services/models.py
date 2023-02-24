bd90802d022102f9c560df179c70dd83c65817c3
hottabych <varvar1987a@mail.ru>
2023-02-24 18:33:09 +0300

    Day 4-1 add models field and function save, task "set_price"

--- service/services/models.py
+++ service/services/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 from clients.models import Client
-
+from .tasks import set_price
 
 class Service(models.Model):
     name = models.CharField(max_length=50)
@@ -29,3 +29,10 @@ class Subscription(models.Model):
     client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
     service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
+    price = models.PositiveIntegerField(default=0)
+
+    def save(self, *args, save_model=True, **kwargs):
+        if save_model:
+            set_price.delay(self.id)
+
+        return super().save(*args, **kwargs)
